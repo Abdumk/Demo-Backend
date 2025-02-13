7a99c3a5d287f649430ff208ae6122506e77aa50
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-02-13 23:11:20 +0300

    server side http verbs done

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 const express = require('express');
 // Import the mysql module  
 const mysql = require('mysql2');
+const cors = require('cors');
+
+
+
 // Create the express app 
 const app = express();
+
+// Use the CORS middleware
+app.use(cors());
 
 // Define the connection parameters for the database 
 const dbConfig = {
@@ -27,9 +34,60 @@ const dbConfig = {
     res.send('Testing!');
   });
 
-
-
-
+// Allow CORS to all 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+  // Post request handler to add a new employee to the database
+  app.post('/add-employee', (req, res) => {
+    // console.log(req.body);
+    // Write the sql query to add to the database table named employee_test
+    const sql = `INSERT INTO employee_test (first_name, last_name, email, password) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${req.body.password}')`;
+    // Execute the query 
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+    // Send a response back to the client 
+    const response = {
+      status: 'success',
+      message: 'Employee added succesfully',
+    };
+    res.status(200).json(response);
+  });
+  // Post request handler to login an employee which comes to this route /login 
+  app.post('/login', (req, res) => {
+    console.log(req.body);
+    // Write the sql query to retrieve the employee with the email and password provided by the user and compare it with the data in the database 
+    const sql = `SELECT * FROM employee_test WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
+    // Execute the query
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      // Check if the result is empty or not 
+      if (result.length > 0) {
+        // Send a response back to the client 
+        const response = {
+          status: 'success',
+          message: 'Login successful',
+        };
+        res.status(200).json(response);
+      } else {
+        // Send a response back to the client 
+        const response = {
+          status: 'failure',
+          message: 'Login failed',
+        };
+        res.status(200).json(response);
+      }
+    });
+  });
 
 
 
