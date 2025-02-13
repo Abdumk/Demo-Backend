edae4346df845fee6436586bbefa81fd49ad22ff
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-02-13 21:27:55 +0300

    starting and install main modules

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the express module 
+const express = require('express');
+// Import the mysql module  
+const mysql = require('mysql2');
+// Create the express app 
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
