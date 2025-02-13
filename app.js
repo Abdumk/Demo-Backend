d0d54c56716060a0e00b7681f1c828c15d2942ee
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-02-13 21:35:25 +0300

    create and connect Databse

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ const mysql = require('mysql2');
 // Create the express app 
 const app = express();
 
-
+// Define the connection parameters for the database 
+const dbConfig = {
+    connectionLimit: 10,
+    password: "demoapp",
+    user: "demoapp",
+    host: "127.0.0.1",
+    database: "demoapp",
+  }
+  // Create the connection to the database 
+  const connection = mysql.createConnection(dbConfig);
+  // Connect to the database 
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  // Use the express.json() middleware to parse the request body 
+  app.use(express.json());
+  // Create a simple get request handler to send a response back  
+  app.get('/', (req, res) => {
+    res.send('Testing!');
+  });
 
 
 
